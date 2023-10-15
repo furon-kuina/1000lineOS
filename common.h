74912b357d33ee9133e1ea8f1fc45bbdaa3f4fdd
Furon-Kuina <tenma.edamura@gmail.com>
2023-10-15 18:57:45 +0900

    feature: implement memory allocation

--- common.h
+++ common.h
@@ -36,3 +36,5 @@ void *memset(void *buf, char c, size_t n);
 void *memcpy(void *dst, const void *src, size_t n);
 char *strcpy(char *dst, const char *src);
 int strcmp(const char *s1, const char *s2);
+
+#define PAGE_SIZE 4096
